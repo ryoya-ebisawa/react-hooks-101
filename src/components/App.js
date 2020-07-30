291e3b2fce280a7cdcecaaa94cabd11099bd6b84
ryoya-ebisawa <ryoya.ebisawa@gmail.com>
2020-07-30 18:29:09 +0900

    削除時の確認

--- src/components/App.js
+++ src/components/App.js
@@ -18,6 +18,17 @@ const App = () => {
     setBody("");
   };
 
+  const deleteAllEvents = (e) => {
+    e.preventDefault();
+    const result = window.confirm("全てのイベントを削除しても良いですか？");
+    if (result) {
+      dispatch({
+        type: "DELETE_ALL_EVENTS",
+      });
+    }
+  };
+
+  const unCreatable = title === "" || body === "";
   return (
     <div className="container-fluid">
       <h4>イベント作成フォーム</h4>
@@ -40,10 +51,20 @@ const App = () => {
             onChange={(e) => setBody(e.target.value)}
           />
         </div>
-        <button className="btn btn-primary" onClick={addEvent}>
+        <button
+          className="btn btn-primary"
+          onClick={addEvent}
+          disabled={unCreatable}
+        >
           イベントを作成する
         </button>
-        <button className="btn btn-danger">全てのイベントを削除する</button>
+        <button
+          className="btn btn-danger"
+          onClick={deleteAllEvents}
+          disabled={state.length === 0}
+        >
+          全てのイベントを削除する
+        </button>
       </form>
       <h4>イベント一覧</h4>
       <table className="table table-hover">
