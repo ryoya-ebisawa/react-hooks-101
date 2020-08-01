3d95c681c062fb214290ad7a0b1dd2a2dfa7bf6a
ryoya-ebisawa <ryoya.ebisawa@gmail.com>
2020-08-01 15:01:24 +0900

    replace type names with constans

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { DELETE_EVENT } from "../actions";
 
 const Event = ({ event, dispatch }) => {
   const id = event.id;
@@ -6,7 +7,7 @@ const Event = ({ event, dispatch }) => {
     const result = window.confirm(`イベント(id=${id})を削除しても良いですか？`);
     if (result) {
       dispatch({
-        type: "DELETE_EVENT",
+        type: DELETE_EVENT,
         id: id,
       });
     }
